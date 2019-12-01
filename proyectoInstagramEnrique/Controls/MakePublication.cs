4d494d2e16fc5542282a3e7174305d404318a273
Enrique Alonso <0198748@up.edu.mx>
2019-11-30 23:45:29 -0600

    Added Create Publication and Read Publication Panels

---
+++ proyectoInstagramEnrique/Controls/MakePublication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace proyectoFinal.Controls
+{
+    public partial class MakePublication : UserControl
+    {
+        public MakePublication()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "JPG (.jpg)|*.jpg|BMP (.bmp)|*.bmp|GIF (.gif)|*.gif|PNG (.png)|*.png";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ proyectoInstagramEnrique/Controls/ViewPublication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using proyectoFinal.Classes;
+namespace proyectoFinal.Controls
+{
+    public partial class ViewPublication : UserControl
+    {
+        public ViewPublication()
+        {
+            InitializeComponent();
+        }
+
+        private User user;
+        private UserService thisUS;
+        private Publication thisPub;
+        public void UpdateView(Publication pub, UserService uS, User u)
+        {
+            thisPub = pub;
+            thisUS = uS;
+            user = u;
+            this.pictureBox1.Image = pub.image;
+            this.label1.Text = thisUS.getLikes(pub.id).ToString()+" likes";
+            if (thisUS.liked(pub.id, user)) label2.ForeColor = Color.Pink;
+            else label2.ForeColor = Color.Black;
+        }
+    }
+}
